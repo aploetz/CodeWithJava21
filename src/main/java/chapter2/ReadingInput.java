e382a1a25d2e20fa8bff3d561ddca746319a16a2
Aaron Ploetz <aaron.ploetz@target.com>
2023-04-18 09:15:32 -0500

    changes for try/catch and if/else.

--- src/main/java/chapter2/ReadingInput.java
+++ src/main/java/chapter2/ReadingInput.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 public class ReadingInput {
 
 	public static void main(String[] args) {
-		System.out.print("Enter a number between 0 and 255: ");
+		System.out.print("Enter a number between 31 and 256: ");
 
 		Scanner inputScanner = new Scanner(System.in);
 		String inputStr = inputScanner.nextLine();
 		inputScanner.close();
 
-		int number = Integer.parseInt(inputStr);
-		
-		System.out.printf("The character for ASCII code %d is %c", number, (char) number);
+		try {
+			int number = Integer.parseInt(inputStr);
+			
+			//if (number > 31 && number < 256) {
+			if (number > 31) {
+				System.out.printf("The character for ASCII code %d is %c", number, (char) number);
+			} else {
+				System.out.println("Sorry, only numbers 32 or higher are permitted.");
+			}
+		} catch (NumberFormatException ex) {
+			System.out.println("Sorry, only numbers are permitted.");
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
-
 }

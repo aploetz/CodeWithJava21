fe650e6357b82d074b497b7952652af1c709db96
Aaron Ploetz <aaron.ploetz@target.com>
2023-05-05 07:38:01 -0500

    additional examples for chapter 3

--- src/main/java/chapter3/WorkingWithStrings.java
+++ src/main/java/chapter3/WorkingWithStrings.java
@@ -15,10 +15,27 @@ public class WorkingWithStrings {
 		String lastName = email.substring(dotPos + 1, atPos);
 		String company = email.substring(atPos + 1, dot2Pos);
 		
+		firstName = properCase(firstName);
+		lastName = properCase(lastName);
+		
 		System.out.println("First name: " + firstName);
 		System.out.println("Last name: " + lastName);
 		System.out.println("Company: " + company);
+		System.out.println();
+		
+		if (firstName.equals("Victoria")) {
+			System.out.println("Your name is Victoria!");
+		} else {
+			System.out.println("Sorry, your name is NOT Victoria.");
+		}
 		
 	}
 
+	private static String properCase(String name) {
+		// grab the first character and convert it to uppercase
+		char firstLetter = Character.toUpperCase(name.charAt(0));
+		
+		// return firstLetter concatenated with the substring of name from position 1 through to the end.
+		return firstLetter + name.substring(1);
+	}
 }

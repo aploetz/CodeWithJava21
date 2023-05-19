9c6d79f0b32d4511b7e1757119a0b1383e5a382d
Aaron Ploetz <aaron.ploetz@target.com>
2023-05-19 15:52:49 -0500

    added printCollections method and material for LinkedList

--- src/main/java/chapter4/WorkingWithCollections.java
+++ src/main/java/chapter4/WorkingWithCollections.java
@@ -1,9 +1,11 @@
 package chapter4;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -23,7 +25,8 @@ public class WorkingWithCollections {
 		
 		heroSet.remove("Rik");
 		
-		System.out.println(heroSet);
+		//System.out.println(heroSet);
+		printCollection(heroSet);
 		
 		List<String> monsterList = new ArrayList<>();
 		monsterList.add("Kobald");
@@ -34,8 +37,28 @@ public class WorkingWithCollections {
 
 		Collections.sort(monsterList);
 		
-		System.out.println(monsterList);
+		monsterList.remove("Skeleton");
+		
+		printCollection(monsterList);
+		System.out.println(monsterList.get(1));
+		
+		List<String> cityList = new LinkedList<>();
+		cityList.add("Elddim");
+		cityList.add("Crystwind");
+		cityList.add("Fallraen");
+		cityList.add("Meren");
+		cityList.add("Lang");
+		
+		printCollection(cityList);
 
 	}
 
+	private static void printCollection(Collection collection) {
+		
+		for (Object element : collection) {
+			System.out.printf("%s, ", element.toString());
+		}
+		
+		System.out.println();
+	}
 }
